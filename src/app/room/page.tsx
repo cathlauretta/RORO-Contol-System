6f94935534ae6c313ea502bd14f17aa6778106db
cathlauretta <18221157@std.stei.itb.ac.id>
2023-11-20 13:18:58 +0700

    feat: add NavPage and add Room

--- src/app/room/page.tsx
+++ src/app/room/page.tsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import { Navbar } from '@/components/Navbar'
+import { NavPage } from '@/components/NavPage'
+import { Flex } from '@chakra-ui/react'
 
-export default function RoomPage() {
+export default function Home() {
   return (
-    <div>RoomPage</div>
+    <Flex width={"100vw"} flexDir={'column'}>
+      <Navbar/>
+      <Flex
+        pt = {40}
+        pb = {40}
+        alignItems={'center'}
+        justify={'center'}
+      >
+        <NavPage active='Rooms' isAdmin={true}/>
+      </Flex>
+    </Flex>
   )
 }
---
+++ src/components/NavPage.tsx
@@ -0,0 +1,70 @@
+'use client';
+
+import React, { useState, useEffect } from 'react'
+import Image from 'next/image';
+import {
+    Flex,
+    Text,
+    Menu,
+    MenuButton,
+    MenuList,
+    MenuItem,
+} from '@chakra-ui/react';
+
+const NAVBAR_HEIGHT = '50px';
+
+const HeadingProp = ({url, text, state} : {url: string, text: string, state: boolean}) => {
+    return (
+        <a href={url}>
+            {
+                state ?
+                    <Flex
+                    textColor={'#1E6DAC'}
+                    fontSize={'28px'}
+                    fontWeight={'600'}
+                    fontStyle={'normal'}
+                    cursor={'pointer'}
+                    minH={"55px"}
+                    pl={12}
+                    pr={12}
+                    borderBottom={'5px solid #1E6DAC'}
+                    >
+                        {text}
+                    </Flex>
+                :
+                    <Flex
+                    textColor={'#87C4FF'}
+                    fontSize={'28px'}
+                    fontWeight={'600'}
+                    fontStyle={'normal'}
+                    cursor={'pointer'}
+                    minH={"55px"}
+                    >
+                        {text}
+                    </Flex>
+            }
+        </a>
+    )
+}
+
+export const NavPage = ({active, isAdmin} : {active: string, isAdmin: boolean}) => {
+    return (
+        <Flex
+            width='85vw'
+            justifyContent=''
+            alignItems={'center'}
+            bgColor={'white'}
+            px={32}
+            height={NAVBAR_HEIGHT}
+            gap={60}
+            borderBottom={'2px solid #E0F4FF'}
+        >
+            <HeadingProp url='/room' text='Rooms' state={active=='Rooms'}/>
+            <HeadingProp url='/report' text='Reports' state={active=='Reports'}/>
+            {
+                isAdmin &&
+                <HeadingProp url='/employee' text='Employee' state={active=='Employee'}/>
+            }
+        </Flex>
+    )
+}
