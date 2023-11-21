9e7b65a01b7d4fea4de4b5f9d81b6c8842f67fb5
Ferdeh <16521246@mahasiswa.itb.ac.id>
2023-11-21 23:10:40 +0700

    feat: Managers
    - Created basic CRUD Managers for employee, report, and room
    - Adjust prisma schema with database

---
+++ src/pages/api/reportManager.ts
@@ -0,0 +1,66 @@
+import prisma from '../../../prisma/client';
+
+export async function ReportGET(report_id:string,report_title:string,room_repaired:string,date:string,status:string){
+    try {
+        const filters = {};
+        if (report_id)          {Object.assign(filters, {report_id: {equals: report_id}})}
+        if (report_title)       {Object.assign(filters, {report_title: {contains: report_title, mode: 'insensitive'}})}
+        if (room_repaired)      {Object.assign(filters, {room_repaired: {contains: room_repaired}})}
+        if (date)               {Object.assign(filters, {date: {contains: date}})}
+        if (status)             {Object.assign(filters, {status: {contains: status}})}
+        const reports = await prisma.report.findMany({where:filters});
+        return reports;
+    }
+    catch (error) {
+        throw error;
+    }
+}
+
+export async function ReportPOST(report_id:string,date:string,room_repaired:string,eic:string,repair_description:string,report_title:string,status:string){
+    const data = {report_id: report_id,
+        date: date,
+        room_repaired: room_repaired,
+        eic: eic,
+        repair_description: repair_description,
+        report_title: report_title,
+        status: status}
+
+    try {
+        const report = await prisma.report.create({data});
+        return report;
+    }
+    catch (error) {
+        throw error;
+    }
+}
+
+export async function ReportPUT(report_id:string,date:string,room_repaired:string,eic:string,repair_description:string,report_title:string,status:string){
+    try {
+        const data = {
+            report_id: report_id,
+            date: date,
+            room_repaired: room_repaired,
+            eic: eic,
+            repair_description: repair_description,
+            report_title: report_title,
+            status: status
+        };
+
+        const updatedReport = await prisma.report.update({
+            where: { report_id: report_id },
+            data: data
+        });
+    } catch (error) {
+        throw error;
+    }
+}
+
+export async function ReportDELETE(report_id:string){
+    try {
+        const deletedReport = await prisma.report.delete({
+            where: { report_id: report_id },
+        });
+    } catch (error) {
+        throw error;
+    }
+}
---
+++ src/pages/api/roomManager.ts
@@ -0,0 +1,73 @@
+import prisma from '../../../prisma/client';
+
+export async function RoomGET(room_id:string,tipe:string,floor:string,flag:string)
+{
+    try {
+        const filters = {};
+        if (room_id)    {Object.assign(filters, {room_id: {equals: room_id}})}
+        if (tipe)       {Object.assign(filters, {tipe: {contains: tipe}})}
+        if (floor)      {Object.assign(filters, {floor: {contains: floor}})}
+        if (flag)       {Object.assign(filters, {flag: {contains: flag}})}
+        
+        const rooms = await prisma.room.findMany({where:filters});
+        return rooms;
+    }
+    catch (error) {
+        throw error;
+    }
+}
+
+export async function RoomPOST(room_id:string,tipe:string,floor:number,price:number,occupied_status:boolean,condition:string,flag:boolean,image:string,repair_notes:string,room_name:string){
+    const data = {room_id: room_id,
+        tipe: tipe,
+        floor: floor,
+        price: price,
+        occupied_status: occupied_status,
+        condition: condition,
+        flag: flag,
+        image: image,
+        repair_notes: repair_notes,
+        room_name: room_name}
+
+    try {
+        const room = await prisma.room.create({data});
+        return room;
+    }
+    catch (error) {
+        throw error;
+    }
+}
+
+export async function RoomPUT(room_id:string,tipe:string,floor:number,price:number,occupied_status:boolean,condition:string,flag:boolean,image:string,repair_notes:string,room_name:string){
+    try {
+        const data = {
+            room_id: room_id,
+            tipe: tipe,
+            floor: floor,
+            price: price,
+            occupied_status: occupied_status,
+            condition: condition,
+            flag: flag,
+            image: image,
+            repair_notes: repair_notes,
+            room_name: room_name
+        };
+
+        const updatedRoom = await prisma.room.update({
+            where: { room_id: room_id },
+            data: data
+        });
+    } catch (error) {
+        throw error;
+    }
+}
+
+export async function RoomDELETE(room_id:string){
+    try {
+        const deletedRoom = await prisma.room.delete({
+            where: { room_id: room_id },
+        });
+    } catch (error) {
+        throw error;
+    }
+}
